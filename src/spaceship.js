edbd1ee20d6dc5bfb5602f41fdf3aa0957fa5c66
aubreeabril <aubree.abril@gmail.com>
2018-09-02 20:34:37 -0400

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,18 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew) {
+    this.name = name
+    this.crew = crew
+    this.phasers = 5
+    this.shields = 4
+    this.cloaked = false
+    this.docked = true
+    this.warpDrive = 'disengaged'
+    this.phasersCharge = 'uncharged'
+    if (this.crew.length === 0) {
+      this.docked
+    } else {
+      this.docked = false
+      this.crew.forEach(m => m.currentShip = this)
+    }
+  }
+}
